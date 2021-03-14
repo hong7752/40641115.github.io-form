580e6b96ba74df894f9d79492cc9b13648708248
hong7752 <70906642+hong7752@users.noreply.github.com>
2021-03-14 21:36:16 +0800

    Add files via upload

---
+++ check.php
@@ -0,0 +1,16 @@
+<!-- 後端程式 -->
+<?php
+echo $_POST["e-mail"] . "<br>";
+// echo $_POST["directions"]."<br>";
+echo $_POST["degree"] . "<br>";
+echo $_POST["msg"] . "<br>";
+
+// if ($_POST["e-mail"] == "root" && $_POST["password"] == "123") {
+//     header("Location:https://www.nfu.edu.tw");
+// }
+// else{
+//     header("Location:index.html");
+// }
+// echo $_POST["e-mail"] . "<br>";
+// echo $_POST["password"] . "<br>";
+?>
